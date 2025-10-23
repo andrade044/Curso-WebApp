d15e54018a65b4bde5e6a9947b11b804d7fd8a1a
andrade044 <matheusandrade906@gmail.com>
2025-10-23 19:27:29 -0400

    add data.py

---
+++ data.py
@@ -0,0 +1,18 @@
+# --- Dados do Simulado (Quiz) ---
+SIMULADO_DATA = [
+    {
+        "pergunta": "Qual é a principal função do BCrypt no sistema de login?",
+        "opcoes": ["Criptografar todo o tráfego do site.", "Gerar hashes irreversíveis para senhas.", "Criptografar dados sensíveis no banco de dados.", "Proteger contra ataques DDOS."],
+        "resposta_correta": "Gerar hashes irreversíveis para senhas."
+    },
+    {
+        "pergunta": "Qual técnica protege o código contra SQL Injection, usada no seu app?",
+        "opcoes": ["Concatenação de Strings.", "Uso de st.expander.", "Consultas Parametrizadas (Prepared Statements).", "Criptografia de ponta-a-ponta."],
+        "resposta_correta": "Consultas Parametrizadas (Prepared Statements)."
+    },
+    {
+        "pergunta": "Qual módulo do Streamlit é ideal para exibir conteúdo condicionalmente (como Módulos Pagos)?",
+        "opcoes": ["st.video", "st.expander", "st.session_state", "st.form"],
+        "resposta_correta": "st.session_state"
+    }
+]
