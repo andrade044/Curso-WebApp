f7a544f6668109bea826b4cd3f7ac37b4cd4b78e
andrade044 <matheusandrade906@gmail.com>
2025-11-05 11:33:14 -0400

    .

--- pages/4_Curso.py
+++ pages/4_Curso.py
@@ -50,19 +50,7 @@ st.set_page_config(
 # </style>
 # """, unsafe_allow_html=True)
 
-st.markdown("""
-<style>
-    /* Esconde o link da página de Cadastro (Página 2) */
-    [data-testid="stSidebarNav"] a[href*="CADASTRO"] {
-        display: none !important;
-    }
 
-    /* Esconde o link da página de Recuperação de Senha (Página 7) */
-    [data-testid="stSidebarNav"] a[href*="rec_senha"] {
-        display: none !important;
-    }
-</style>
-""", unsafe_allow_html=True)
     
 # --- Configuração do Banco de Dados SQLite ---
 
